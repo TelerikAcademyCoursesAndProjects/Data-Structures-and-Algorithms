014e6fba345659cbfce04c535eaf6dfb3ded0903
shopov91 <shopov91@gmail.com>
2016-11-19 23:54:15 +0200

    Task 1 done

--- 02. Linear-Data-Structures/TelerikDSALinearDataStructuresHW/Task1ReadSequenceOfNumbers/StartUp.cs
+++ 02. Linear-Data-Structures/TelerikDSALinearDataStructuresHW/Task1ReadSequenceOfNumbers/StartUp.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task1ReadSequenceOfNumbers
 {
-    class StartUp
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+
+            BigInteger avarageOfNumbers = 0;
+        }
+
+        internal List<int> ReadingConsoleInput()
+        {
+            var input = Console.ReadLine();
+            var listOfNumbers = new List<int>();
+
+            while (input != string.Empty)
+            {
+                listOfNumbers.Add(int.Parse(input));
+                input = Console.ReadLine();
+            }
+
+            return listOfNumbers;
+        }
+        internal BigInteger SumingNumbers(List<int> listOfNumbers)
+        {
+            BigInteger sumOfNumbers = 0;
+            sumOfNumbers = listOfNumbers.Sum();
+
+            return sumOfNumbers;
         }
     }
 }
