8430ac4eea6e5bd5fa629d223b5ef1c0bf22213c
shopov91 <shopov91@gmail.com>
2016-11-21 17:19:53 +0200

    Task 9 implemented

---
+++ 02. Linear-Data-Structures/TelerikDSALinearDataStructuresHW/Task9PrintFirst50Members/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9PrintFirst50Members
+{
+   public class StartUp
+    {
+        internal static Queue<int> SequenceCreator(int number)
+        {
+            int numberOfMembersToPrint = 50;
+
+            var container = new Queue<int>();
+            container.Enqueue(number);
+            var result = new Queue<int>();
+
+            for (int i = 0; i < numberOfMembersToPrint; i++)
+            {
+                int currentBase = container.Dequeue();
+                result.Enqueue(currentBase);
+                container.Enqueue(currentBase + 1);
+                container.Enqueue(2 * currentBase + 1);
+                container.Enqueue(currentBase + 2);
+            }
+
+            return result;
+        }
+       public static void Main()
+        {
+            Console.Write("Enter number N = ");
+            var input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}",string.Join(", ",SequenceCreator(input)));
+        }
+    }
+}
