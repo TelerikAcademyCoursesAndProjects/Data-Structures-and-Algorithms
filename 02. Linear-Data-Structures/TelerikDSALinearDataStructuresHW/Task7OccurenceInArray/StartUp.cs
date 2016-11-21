53dcd5ef8803570362932deec1880386bfd6e247
shopov91 <shopov91@gmail.com>
2016-11-21 15:41:58 +0200

    Task 8 implemeneted

--- 02. Linear-Data-Structures/TelerikDSALinearDataStructuresHW/Task7OccurenceInArray/StartUp.cs
+++ 02. Linear-Data-Structures/TelerikDSALinearDataStructuresHW/Task7OccurenceInArray/StartUp.cs
@@ -12,13 +12,13 @@ namespace Task7OccurenceInArray
         {
             Console.WriteLine("Enter integer numbers on a single line separated by comma (,)");
             var sequenceOfNumbers = Console.ReadLine().Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-            var numbersThatOccurEvenNumberOfTimes = sequenceOfNumbers
+            var numbersAndOccurenceTimes = sequenceOfNumbers
                 .GroupBy(x => x)
                 .OrderBy(x => x.Key);
 
             Console.WriteLine("The numbers that occurs and how many times it occurs: ");
 
-            foreach (var group in numbersThatOccurEvenNumberOfTimes)
+            foreach (var group in numbersAndOccurenceTimes)
             {
                 Console.WriteLine("{0} -> {1} times", group.Key, group.Count());
             }
