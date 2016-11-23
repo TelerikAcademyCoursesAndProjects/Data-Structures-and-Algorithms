052ec167c13f882a0a01897b884d82c1f9519c88
shopov91 <shopov91@gmail.com>
2016-11-23 23:56:48 +0200

    Task 1 still not implemented

---
+++ 07. Recursion/homework/TelerikDSARecursion/Task1NNestedLoops/NestedLoopsStartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1NNestedLoops
+{
+    public class NestedLoopsStartUp
+    {
+        public static void NestedLoopsSimulationWithRecursion(int number)
+        {
+            int startNumber = 1;
+            int endNumber = number;
+            int[,] matrix = new int[endNumber, endNumber];
+
+                matrix[number, number] = startNumber;
+
+                NestedLoopsSimulationWithRecursion(number);
+        }
+
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            NestedLoopsSimulationWithRecursion(number);
+        }
+    }
+}
