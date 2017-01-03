5014c81f0bd420d2d34079f2d6f7af1626114afe
shopov91 <shopov91@gmail.com>
2017-01-03 22:14:40 +0200

    Recursion homework added

--- 07. Recursion/homework/TelerikDSARecursion/02.CombinationsWithDuplicat/Properties/AssemblyInfo.cs
+++ 07. Recursion/homework/TelerikDSARecursion/02.CombinationsWithDuplicat/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task1NNestedLoops")]
+[assembly: AssemblyTitle("02.CombinationsWithDuplicat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1NNestedLoops")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("02.CombinationsWithDuplicat")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ca38320-c843-4597-ab5c-aa56fc4967ad")]
+[assembly: Guid("28243c75-e1c2-4b6d-a2e9-e4a7e1ce47aa")]
 
 // Version information for an assembly consists of the following four values:
 //
