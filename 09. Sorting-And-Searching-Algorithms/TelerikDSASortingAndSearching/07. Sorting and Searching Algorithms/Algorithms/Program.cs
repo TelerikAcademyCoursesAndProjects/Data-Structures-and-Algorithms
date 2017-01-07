dd6f249a3d6a8bf782f53ad2af1d8d06cca61fbe
shopov91 <shopov91@gmail.com>
2017-01-07 13:35:21 +0200

    Added homework

--- 09. Sorting-And-Searching-Algorithms/TelerikDSASortingAndSearching/07. Sorting and Searching Algorithms/Algorithms/Program.cs
+++ 09. Sorting-And-Searching-Algorithms/TelerikDSASortingAndSearching/07. Sorting and Searching Algorithms/Algorithms/Program.cs
@@ -1,10 +1,7 @@
-﻿namespace SortingHomework
+﻿namespace Algorithms
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class Program
     {
@@ -13,37 +10,30 @@
             var collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
             Console.WriteLine("All items before sorting:");
             collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
 
             Console.WriteLine("SelectionSorter result:");
             collection.Sort(new SelectionSorter<int>());
             collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
 
             collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
             Console.WriteLine("Quicksorter result:");
-            collection.Sort(new Quicksorter<int>());
+            collection.Sort(new QuickSorter<int>());
             collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
 
             collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
             Console.WriteLine("MergeSorter result:");
             collection.Sort(new MergeSorter<int>());
             collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
 
             Console.WriteLine("Linear search 101:");
-            Console.WriteLine(collection.LinearSearch(101));
-            Console.WriteLine();
+            Console.WriteLine(collection.LinearSearch(101) + Environment.NewLine);
 
             Console.WriteLine("Binary search 101:");
-            Console.WriteLine(collection.BinarySearch(101));
-            Console.WriteLine();
+            Console.WriteLine(collection.BinarySearch(101) + Environment.NewLine);
 
             Console.WriteLine("Shuffle:");
             collection.Shuffle();
             collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
 
             Console.WriteLine("Shuffle again:");
             collection.Shuffle();
